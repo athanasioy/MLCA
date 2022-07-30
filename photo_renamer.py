bf8d1fcc075e51603ed30ef43d0f4a0605ced063
athanasioy <athanasioy.antonis@gmail.com>
2022-07-31 01:01:41 +0300

    Initial Commit
    compress_images.py -> script that compressed images
    photo_renamer.py -> renames the photo based on the folder in which they are placed.
    						  The folder name represents class label
    rename_to_uppercase -> script to change file extensions

---
+++ photo_renamer.py
@@ -0,0 +1,79 @@
+'''
+This module contains the basic classes that will transform the
+data into usable format for the neural netowork to be trained
+'''
+
+import os
+import glob
+from typing import List
+
+
+
+def path_extractor(path: str, path_seperator: str = "/") -> str:
+	"""
+	Takes a Path as input and removes the last part,
+	e.g. /home/media/example.jpg -> /home/media
+	"""
+	return path_seperator.join(path.split(path_seperator)[:-1])
+
+def full_path(path: str) -> str:
+	"""
+	Returns the full (absolute path). Expects as input a relative path
+	"""
+	cwd = os.getcwd()
+	return os.path.join(cwd,path)
+
+def new_image_name(path: str, class_name: str, counter: int = 0, file_format: str = "jpg") -> str:
+	"""
+	The function takes the relative path of a file and returns a new path
+	with the file renamed as "{class_name}_{counter}_{file_format}"
+	"""
+	
+	photo_path = path_extractor(path)
+	photo_path = full_path(photo_path)
+	new_name = class_name+"_"+str(counter)+file_format
+	return os.path.join(photo_path,new_name)
+
+class photoRenamer(object):
+	"""
+	This Class takes photo file paths which are inside folders.
+	The folder name depicts label that the image represents.
+	With the .rename method, the images are renamed to
+	{class_name}_{counter}_{file_format} 
+	"""
+
+	def __init__(self, photo_paths: List[str]):
+		self.photos = photo_paths
+		
+
+	def get_class_names_from_folders(self, path_seperator: str = "/") -> List[str]:
+		"""
+		This method assumes that photo class names are
+		located in the name of the folder. The returned List
+		has equal length to the self.photo List.
+		"""
+		self.classes = [path.split(path_seperator)[-2] for path in self.photos]
+	
+
+	def rename_photos(self):
+		counter = 0
+		temp = "" # Used for checking when class name changes
+		for old_image_name,class_name in zip(self.photos,self.classes):
+
+			image_name_new = new_image_name(old_image_name,class_name,counter)
+
+			os.rename(old_image_name,image_name_new)
+			if temp == class_name: 
+				counter += 1
+			else: # if class_name changes, reset counter
+				counter = 0
+			temp = class_name
+
+
+if __name__ == "__main__":
+
+
+	file_paths = glob.glob("**/*.JPG",recursive = True)
+	renamer = photoRenamer(file_paths)
+	renamer.get_class_names_from_folders() # Get Classes from Folders
+	renamer.rename_photos()
